71430af6223f9bddacf400a1043df70038b02f5b
liu-congcong <liu_congcong@icloud.com>
2025-05-20 13:43:54 +0800

    v1.0.1

--- img2datetime.c
+++ img2datetime.c
@@ -4,34 +4,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <time.h>
 
 #include "heic.h"
 #include "jpeg.h"
 
 int printHelp()
 {
-    puts("img2datetime v1.0.0");
+    puts("img2datetime v1.0.1");
     puts("Rename images using their creation time.");
     puts("https://github.com/liu-congcong/img2datetime");
     puts("\nUsage:");
     puts("  img2datetime [options]");
     puts("\nOptions:");
-    puts("  -i    Input directory containing the images.\n");
+    puts("  -i    Input directory containing the images.");
+    puts("  -o    Output directory. Default: input directory.\n");
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
     char *input = NULL;
-    for (int i = 1; i < argc; i++)
+    char *output = NULL;
+    for (int i = 1; i < argc - 1; i++)
     {
         if (!strncmp("-i", argv[i], 2)) input = argv[i + 1];
+        else if (!strncmp("-o", argv[i], 2)) output = argv[i + 1];
     }
     if (!input)
     {
         printHelp();
         exit(EXIT_FAILURE);
     }
+    if (!output) output = input;
+
     struct dirent *entry = NULL;
     struct stat statBuffer;
     char *ext = NULL;
@@ -39,6 +45,8 @@ int main(int argc, char *argv[])
     char *buffer1 = malloc(10240 * sizeof(char)); /* path to input img */
     char *buffer2 = malloc(10240 * sizeof(char)); /* path to output img */
     char buffer3[16]; /* datetime */
+    struct timespec ts;
+    long long nanoseconds;
 
     DIR *dir = opendir(input);
     assert(dir);
@@ -51,20 +59,17 @@ int main(int argc, char *argv[])
             {
                 if (!S_ISDIR(statBuffer.st_mode))
                 {
-                    memset(buffer3, 0, 16);
+                    strcpy(buffer3, "00000000-000000");
                     ext = strrchr(entry->d_name, '.');
                     if (!strncasecmp(ext, ".jpg", 4) || !strncasecmp(ext, ".jpeg", 5)) readJPEG(buffer1, buffer3);
                     else if (!strncasecmp(ext, ".heic", 5)) readHEIC(buffer1, buffer3);
                     else continue;
-                    if (strlen(buffer3) && strncmp(entry->d_name, buffer3, 15))
-                    {
-                        snprintf(buffer2, 10240, "%s/%s%s", input, buffer3, ext);
-                        if (access(buffer2, F_OK))
-                        {
-                            printf("%s -> %s\n", buffer1, buffer2);
-                            rename(buffer1, buffer2);
-                        }
-                    }
+                    timespec_get(&ts, TIME_UTC);
+                    nanoseconds = (long long)ts.tv_sec * 1000000000LL + (long long)ts.tv_nsec;
+                    snprintf(buffer2, 10240, "%s/%s %lld%s", output, buffer3, nanoseconds, ext);
+                    assert(access(buffer2, F_OK));
+                    printf("%s -> %s\n", buffer1, buffer2);
+                    assert(!rename(buffer1, buffer2));
                 }
             }
         }
